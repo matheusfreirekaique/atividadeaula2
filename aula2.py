b93e3d6f87199c5f05fefbfbbd11c332c762b7ba
Matheus Freire <santosmatheus99@hotmail.com>
2023-02-27 23:01:14 -0300

    atividadeaula2

---
+++ aula2.py
@@ -0,0 +1,17 @@
+# script calacular o imc de um individo 
+# usar variaveis, guardar o resulatado e imprimir esse valor
+# alem do valor informar a condição do individo
+
+
+peso = 63.50
+print ('meu peso e de', peso)
+
+altura = 1.65
+print ('minha altura e de', altura)
+
+imc = peso / (altura **2)
+print('o imc e de {}'.format(imc))
+
+if 18.4 <= imc >23:
+    print ('acima do peso')
+    
---
+++ main.py
@@ -0,0 +1,8 @@
+dog = 'cachorro louco'
+print (dog)
+
+dog_o_animal_coragem = 'esse doc foi \n bom demais'
+print(dog_o_animal_coragem)
+
+criar_pasta = 'abrir pasta \\ criar um nome\\ salvar pasta\\ inserir doc'
+print(criar_pasta)
