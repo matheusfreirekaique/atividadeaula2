7b9c3c7efa3c427c690a56b16248933668aa9d05
Matheus Freire <santosmatheus99@hotmail.com>
2023-03-15 20:38:02 -0300

    Atualização fun3

--- funcoes.py
+++ funcoes.py
@@ -12,9 +12,33 @@ def calc_imc(peso, altura):
         print ('você esta em obesidade')
   elif imc >= 65:
         print ('você esta super obeso cuidado')
+        
+  print()
          
 
 
+def calc_imclist(peso_list, altura_list):
+      list_imc = peso_list /(altura_list * altura_list)
+      print('O list_imc é de:{:.1f}'.format(list_imc))
+      
+      if list_imc < 18.8:
+            print('voce esta abaixo do peso')
+      elif list_imc >= 18.8 < 25:
+        print('voce esta no peso ideal')
+      elif list_imc >= 25 < 45:
+            print ('voce esta acima do peso')
+      elif list_imc >= 45 < 65:
+            print ('voce esta em obesidade')
+      elif list_imc >= 65:
+            print('voce esta super obeso cuidado')
+      
+      print()
+      
+
+      
+     
+opcao1 = calc_imc
+print(input('primeira opção'))
 
 peso_str = input('qual é seu peso: ')
 peso = float(peso_str)
@@ -22,26 +46,22 @@ altura_str = input('qual é a sua altura: ')
 altura = float(altura_str)
 calc_imc(peso, altura)
 
+opcao2= calc_imclist
+print(input('segunda opção'))
 
 
-
-  
-  
-
-
-
-
-
-
-
-
-
+pessoas = [(1.85, 70), (1.50, 68), (1.76, 67)]
+for pessoa in pessoas:
+      peso_list = pessoa[1]
+      altura_list = pessoa[0]
+      calc_imclist(peso_list, altura_list)
+ 
+print('''[1] Entrar com peso e altura 
+      [2] processar lista de pessoas''')
+print('Qual sua opção?')
+      
 
 
-
-
-
-
-
-
-
+# criar intreface com usuario e entrar com o peso e altura 
+#1) Entrar com peso e altura
+#2) Processar lista de pessoass
