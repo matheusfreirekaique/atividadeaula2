1093c511bf8330d3cc603ad89f6bc03a3cda298c
Matheus Freire <santosmatheus99@hotmail.com>
2023-03-27 21:00:51 -0300

    atualização.v

--- funcoes.py
+++ funcoes.py
@@ -15,52 +15,45 @@ def calc_imc(peso, altura):
         
   print()
          
-
-
-def calc_imclist(peso_list, altura_list):
-      list_imc = peso_list /(altura_list * altura_list)
-      print('O list_imc é de:{:.1f}'.format(list_imc))
-      
-      if list_imc < 18.8:
-            print('voce esta abaixo do peso')
-      elif list_imc >= 18.8 < 25:
-        print('voce esta no peso ideal')
-      elif list_imc >= 25 < 45:
-            print ('voce esta acima do peso')
-      elif list_imc >= 45 < 65:
-            print ('voce esta em obesidade')
-      elif list_imc >= 65:
-            print('voce esta super obeso cuidado')
-      
-      print()
           
 
-def opcao( ):     
-      print(calc_imc)
-      print(calc_imclist)
-     
+def opcao1( ):
+      peso_str = input('qual é seu peso: ')
+      peso = float(peso_str)
+      altura_str = input('qual é a sua altura: ')
+      altura = float(altura_str)
+      calc_imc(peso, altura)
+            
       print()
 
 
-print('''[1] Entrar com peso e altura 
-      [2] processar lista de pessoas''')
-print(input('digite a opção:'))
-opcao()
+def opcao2():
+      pessoas = [(1.85, 70), (1.50, 68), (1.76, 67)]
+      for pessoa in pessoas:
+            peso = pessoa[1]
+            altura = pessoa[0]
+            calc_imc(peso, altura)
+      
+def opcao3():
+      pass
+
         
-peso_str = input('qual é seu peso: ')
-peso = float(peso_str)
-altura_str = input('qual é a sua altura: ')
-altura = float(altura_str)
-calc_imc(peso, altura)
+      
+print('''[1] Entrar com peso e altura 
+      [2] processar lista de pessoas
+      [3] batata''')
 
-pessoas = [(1.85, 70), (1.50, 68), (1.76, 67)]
-for pessoa in pessoas:
-      peso_list = pessoa[1]
-      altura_list = pessoa[0]
-      calc_imclist(peso_list, altura_list)
+x = input ('digite uma opção')
 
+if x == '1':
+      opcao1()
+      
+if x == '2':
+      opcao2()
 
+if x == '3':
+      opcao3()
 
-# criar intreface com usuario e entrar com o peso e altura 
+ 
 #1) Entrar com peso e altura
 #2) Processar lista de pessoass
